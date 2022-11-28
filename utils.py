18216e61e34ef4e103cbb62ddc757a13298ab426
Naoufel <naoufel.maazouzi@live.fr>
2022-11-29 00:22:45 +0100

    comment cookie

--- utils.py
+++ utils.py
@@ -8,11 +8,11 @@ def switchWindow(driver, index):
 def goNextPage(driver):
     global cookiesClosed
     print(f"cookiesClosed {cookiesClosed}")
-    cookiesPopup = driver.find_element(By.XPATH, "//*[contains(@class, 'ch2-btn ch2-allow-all-btn ch2-btn-primary ch2-btn-text-xxs')]",)
-    if cookiesPopup and cookiesClosed == False:
-        print("Click on cookies popup !!!")
-        cookiesPopup.click()
-        cookiesClosed = True
+    # cookiesPopup = driver.find_element(By.XPATH, "//*[contains(@class, 'ch2-btn ch2-allow-all-btn ch2-btn-primary ch2-btn-text-xxs')]",)
+    # if cookiesPopup and cookiesClosed == False:
+    #     print("Click on cookies popup !!!")
+    #     cookiesPopup.click()
+    #     cookiesClosed = True
     nextPageBtn = driver.find_element(
                 By.XPATH,
                 "//*[contains(@class, '___SNextPage_12i0x-red-team ___SButton_1c1ei-red-team _size_m_1c1ei-red-team _theme_primary-info_1c1ei-red-team')]",
